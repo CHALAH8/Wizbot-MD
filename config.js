129810febd6bfd265c6500e3443292d80178174f
CHALAH8 <induwarachalana4@gmail.com>
2025-05-30 13:37:41 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PREFIX: process.env.PREFIX || '.',
+    BOT_NAME: process.env.BOT_NAME || 'wa-bot',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94764497078',
+    SESSION_ID: process.env.SESSION_ID || "",
+    ALIVE_IMG: process.env.ALIVE_IMG || "",
+    ALIVE_MSG: process.env.ALIVE_MSG || " Whatsapp User Bot ",
+    AUTO_REPLY: process.env.AUTO_REPLY || "true",
+};
